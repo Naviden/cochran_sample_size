8c5784923157dc909f3ba941927ff753109e6d54
Navid Nobani <navid.nobani@gmail.com>
2020-01-06 14:44:10 +0100

    Update the function

--- sample_size.py
+++ sample_size.py
@@ -1,5 +1,18 @@
-rom scipy.stats import norm
+from scipy.stats import norm
 
-def sample_size(confidence_level, precision, population_size=None):
+def sample_size(confidence_level, precision, population_size=None, round=True, p=0.5):
     Z = norm.ppf(confidence_level)
+    print(Z)
+    n0 = ((Z ** 2) * p * (1-p)) / precision ** 2
+    if population_size is None:
+        if round:
+            n0 = int(n0)
+        return n0
+    else:
+        n = n0 / (1 + (n0 -1) / population_size)
+        if round:
+            n = int(n)
+        return n
+
+print(sample_size(0.95, 0.05))
 
