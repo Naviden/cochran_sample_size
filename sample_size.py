5999a3ab02973b715580bb9fdd927eeddd51cd4f
Navid Nobani <navid.nobani@gmail.com>
2020-01-07 09:36:05 +0100

    Rename precisoin variable

--- sample_size.py
+++ sample_size.py
@@ -1,9 +1,9 @@
 from scipy.stats import norm
 
-def cochran(confidence_level, precision, population_size=None, round=True, p=0.5):
+def cochran(confidence_level, margin_of_error, population_size=None, round=True, p=0.5):
     """Calculate sample size based on Cochran (1977) method """
     Z = norm.ppf(confidence_level)
-    n0 = ((Z ** 2) * p * (1-p)) / precision ** 2
+    n0 = ((Z ** 2) * p * (1-p)) / margin_of_error ** 2
     if population_size is None:
         if round:
             n0 = int(n0)
@@ -14,15 +14,15 @@ def cochran(confidence_level, precision, population_size=None, round=True, p=0.5
             n = int(n)
         return n
 
-def yamane(precision, population_size, round=True):
+def yamane(margin_of_error, population_size, round=True):
 
     """Calculate sample size based on Yamane (1967) method
        Assumption : 95% confidence level and p = 0.5 """
-    n = population_size / (1 + (population_size * (precision ** 2)))
+    n = population_size / (1 + (population_size * (margin_of_error ** 2)))
     if round:
         n = int(n)
     return n
 
 print(cochran(0.95, 0.05))
-print(yamane(0.05, 20_000_000))
+print(yamane(0.05, 60_000_000))
 
